ebf3a939815671942b413d4b0f8441d602302803
your name <553232840@qq.com>
2022-12-01 22:34:21 +0800

    增加编码器

--- seq2seq.py
+++ seq2seq.py
@@ -18,11 +18,40 @@ class Seq2SeqEncoder(d2l.Encoder):
 
 
 
-encoder = Seq2SeqEncoder(vocab_size=10, embed_size=8, num_hiddens=16, num_layers=2)
+class Seq2SeqDecoder(d2l.Decoder):
+    """用于序列到序列学习的循环神经网络解码器"""
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers,
+                 dropout=0, **kwargs):
+        super(Seq2SeqDecoder, self).__init__(**kwargs)
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size + num_hiddens, num_hiddens, num_layers,
+                          dropout=dropout)
+        self.dense = nn.Linear(num_hiddens, vocab_size)
 
-encoder.eval()
+    def init_state(self, enc_outputs, *args):
+        return enc_outputs[1]
+
+    def forward(self, X, state):
+        # 输出'X'的形状：(batch_size,num_steps,embed_size)
+        X = self.embedding(X).permute(1, 0, 2)
+        # 广播context，使其具有与X相同的num_steps
+        context = state[-1].repeat(X.shape[0], 1, 1)
+        X_and_context = torch.cat((X, context), 2)
+        output, state = self.rnn(X_and_context, state)
+        output = self.dense(output).permute(1, 0, 2)
+        # output的形状:(batch_size,num_steps,vocab_size)
+        # state的形状:(num_layers,batch_size,num_hiddens)
+        return output, state
+
 X = torch.zeros((4,7), dtype=torch.long)
-output, state = encoder(X) 
-output.shape 
-state.shape 
+encoder = Seq2SeqEncoder(vocab_size=10, embed_size=8, num_hiddens=16, num_layers=2)
+# encoder.eval()
+decoder = Seq2SeqDecoder(vocab_size=10, embed_size=8, num_hiddens=16,
+                         num_layers=2)
+# decoder.eval()
 
+a,b = encoder(X)
+state = decoder.init_state(encoder(X))
+output, state = decoder(X, state)
+
+print(output.shape, state.shape)
