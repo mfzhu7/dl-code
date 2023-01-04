f16325ce04482c2d430065cf45f8a178430193d5
your name <553232840@qq.com>
2023-01-04 09:17:58 +0800

    'nw'

--- nw_ regression.py
+++ nw_ regression.py
@@ -1,6 +1,7 @@
 import torch 
 from torch import nn 
 from d2l import torch as d2l 
+nn.parameter
 
 
 n_train = 50 
@@ -20,6 +21,47 @@ def plot_kernel_reg(y_hat):
         xlim=[0,5], ylim=[-1, 5])
     d2l.plt.plot(x_train, y_train, 'o', alpha=0.5)
 
-y_hat = torch.repeat_interleave(y_train.mean(), n_test)
-plot_kernel_reg(y_hat)  
+
+
+class NWKernelRegression(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.w = nn.Parameter(torch.rand((1,), requires_grad=True))
+        
+    def forward(self, queries, keys, values):
+        queries = queries.repeat_interleave(keys.shape[1]).reshape((-1, keys.shape[1]))
+        self.attention_weights = nn.functional.softmax(-((queries - keys) * self.w)**2 / 2, dim=1)
+        return torch.bmm(self.attention_weights.unsqueeze(1), values.unsqueeze(-1).reshape(-1))
     
+
+X_title = x_train.repeat((n_train, 1))
+Y_title = y_train.repeat((n_train, 1))
+
+keys = X_title[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
+values = Y_title[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
+
+
+net = NWKernelRegression() 
+loss = nn.MSELoss(reduction='none')
+trainer = torch.optim.SGD(nn.parameters(), lr=0.5)
+animator = d2l.Animator(xlabel='epoch', ylabel='loss', xlim=[1, 5])
+
+for epoch in range(5):
+    trainer.zero_grad()
+    l = loss(net(x_train, keys, values), y_train) / 2 
+    l.sum().backward()
+    trainer.step() 
+    print(f'epoch {epoch + 1}, loss {float(l.sum()):.6f}')
+    animator.add(epoch + 1, float(l.sum()))
+
+keys = x_train.repeat((n_test, 1))
+values = y_train.repeat((n_test, 1))
+y_hat = net(x_test, keys, values).unsqueeze(1).detach()
+plot_kernel_reg(y_hat)
+
+d2l.show_heatmaps(
+    net.attention_weights.unsqueeze(0).unsqueeze(0),
+    xlabel='Sorted training inputs', ylabel='Sorted testing inputs')
+
+
+
